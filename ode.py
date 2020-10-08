1acbb50db431930108b48db6bb019100d2d541a7
axsk <smartmind23@googlemail.com>
2020-10-08 13:07:22 +0200

    implement new softmin

--- ode.py
+++ ode.py
@@ -14,6 +14,7 @@ q(0,x) is the probability to hit set A in the finite timespan [0,T]
 from scipy.integrate import odeint
 import numpy as np
 from scipy.sparse.construct import spdiags
+from matplotlib import pyplot as plt
 
 def finite_time_hitting_prob(Qs, dts, a, tol = 1e-8):
     """ fhtp for state a """
@@ -43,7 +44,7 @@ def finite_time_hitting_probs(Qs, dts) -> np.ndarray:
     return hps.T
 
 class finite_time_hitting_prob_adjoint:
-    def __init__(self, Qs, dts, us, nquad=100):
+    def __init__(self, Qs, dts, us, nquad=500):
         self.Qs = Qs
         self.dts = dts
         self.cts = np.cumsum(dts)
@@ -56,10 +57,6 @@ class finite_time_hitting_prob_adjoint:
         self.its = np.linspace(0, self.cts[-1], nquad)
 
         self.dQs = [dqmultiplier(Qs[i], us[i].flatten()) for i in range(self.nt)]
-
-        self.activei = None
-        self.activej = None
-        self.activey = None
 
     def timeindex(self, t):
         return next((i for i in range(self.nt) if self.cts[i]>t), len(self.cts)-1)
@@ -86,62 +83,97 @@ class finite_time_hitting_prob_adjoint:
     def finite_time_hitting_probs(self):
         # compute all hitting probs and save the active minimium
         # hps[i,j] is the prob. to hit j when starting in i
-        self.hps = np.zeros((self.nx, self.nx))
-        self.hpmin = np.inf
+        hps = np.zeros((self.nx, self.nx))
 
         for j in range(self.nx):
             y = self.ode_y(j)
-            y0 = y[0,:]
-            cmin = np.min(y0)
-            if cmin < self.hpmin:
-                self.hpmin = cmin
-                self.activej = j
-                self.activei = np.argmin(y0)
-                self.activey = y
+            hps[:,j] = y[0,:]
 
-            self.hps[:,j] = y0
-        
-        return self.hps
+        self.hps = hps
+        return hps
 
-    def ode_mu(self):
+    def ode_mu(self, i, j):
         # adjoint ode for the derivative of the active hp
 
         def dmu(mu, t):
-            mu[self.activej] = 0
+            mu[j] = 0  # equivalent to setting the j-th row of Q to 0 
             d = -self.dfdy(t).T.dot(mu)
             d = np.nan_to_num(d)
             return d
 
         mu0 = np.zeros(self.nx)
-        mu0[self.activei] = 1
+        mu0[i] = 1
 
-        self.mu = odeint(dmu, mu0, self.its)
+        mu = odeint(dmu, mu0, self.its)
+        return mu
 
     def dfdp(self, t, y):
         # df/dp = -dQ/dp * y
-        return self.dQs[self.timeindex(t)](y)
+        return  self.dQs[self.timeindex(t)](y)
 
-    def adjointintegrate(self):
+    def adjointintegrate(self, mu, y):
         # int_0^T mu^* f_p 
 
         its = self.its
-        y = self.activey
 
         dg = np.zeros(self.nx)
         for i in range(len(its)-1):
             dt = its[i+1] - its[i]
-            mu = self.mu[i]
             dqy = self.dfdp(its[i], y[i])
-            #dg += -dt * np.dot(mu, dqy)
-            dg += -dt * dqy.T.dot(mu)
+            dg += dt * dqy.T.dot(mu[i])
 
-        self.dg = dg
+        return dg
+
+    def sorthps(self, hps):
+        """ given the hps matrix, return the indices of the minima
+        inds[i,1] -> inds[i,2] is the i-th lowest hitting probability """
+        inds = np.unravel_index(np.argsort(hps, axis=None), shape=(self.nx, self.nx))
+        inds = np.vstack(inds).T
+        return inds
 
     def min_and_derivative(self):
-        self.finite_time_hitting_probs()
-        self.ode_mu()
-        self.adjointintegrate()
-        return self.hpmin, self.dg
+        hps = self.finite_time_hitting_probs()
+        i, j  = self.sorthps(hps)[0,:]
+        dg = self.derivative(i, j)
+        return hps[i,j], dg
+    
+    def derivative(self, i, j):
+        y = self.ode_y(j)
+        mu = self.ode_mu(i, j)
+        dg = self.adjointintegrate(mu, y)
+
+        return dg
+
+    def relaxedminderivative(self, rtol = 0.1, nmax = 10):
+        hps = self.finite_time_hitting_probs()
+        inds = self.sorthps(hps)
+
+        dg = np.zeros((self.nx))
+        hpmin = hps[inds[0,0],inds[0,1]]
+        n = 0
+
+        hpm = []
+        dgs = []
+
+        for ind in inds[0:nmax]:
+            i,j = ind
+            hp = hps[i,j]
+            if hp > hpmin * (1+rtol):
+                break
+
+            hpm.append(hp)
+            dgs.append(self.derivative(i,j))
+        
+        dg = dsoftmin(np.array(hpm)).dot(dgs)
+        hp = softmin(np.array(hpm))
+        if False:
+            print("softmin", hp)
+            print("mins", hpm)
+            print("hardmin", hpmin)
+        return hp, dg
+
+                                    
+            
 
 import scipy.sparse as sp
 
@@ -173,57 +205,162 @@ def dqmultiplier(Q, u, beta=1):
     return multiplier
 
 
-class Rprop:
-    def __init__(self, df, x0, g0=None, inc=1.2, dec=.5, max=50, min=10**(-6)):
-        self.df = df
-        self.x = x0
-        self.lastdf = df(x0)
-        if g0 is None:
-            self.g = np.random.rand(len(x0))
-        else:
-            self.g = g0
+# OLD softmin, did not really work as supposed
+def softmin_p(xs, p=-40):
+    "goes to minimum for negative alpha -> -infty "
+    return np.sum(xs ** p) ** (1/p)
 
-        self.inc = inc
-        self.dec = dec
-        self.max = max
-        self.min = min
-        self.hist_x = [x0]
+def dsoftmin_p(xs, p=-40):
+    #outer = 1/p * (np.sum(xs ** p)) ** (1/p - 1)
+    #inner = p * xs ** (p-1)
+    return np.sum(xs ** p) ** (1/p - 1) * (xs ** (p-1))
 
-    def iterate(self):
-        df = self.df(self.x)
-        g = self.g
-        for i in range(len(g)):
-            if df[i] * self.lastdf[i] > 0:
-                g[i] = np.minimum(g[i] * self.inc, self.max)
-            else:
-                g[i] = np.maximum(g[i] * self.dec, self.min)
 
-        dx = - g * np.sign(df)
-        self.lastdf = df
-        self.x += dx
-        self.hist_x.append(self.x.copy())
+# wrapper around the relative softmin
+def softmin(xs, scale=13):
+    return softmin_rel(xs, scale)
 
-    def run(self, iter):
-        for i in range(iter):
-            self.iterate()
+def dsoftmin(xs, scale=13):
+    return fd_softmin_rel(xs, scale)[1]
+
+# scale invariant version of softmin_e due to log/exp transformation
+# the scale paramter controls the crispness of the softmin
+def softmin_rel(xs, scale=1):
+    return np.exp(softmin_e(np.log(xs) * scale) / scale)
+
+def fd_softmin_rel(xs, scale=1):
+    ls = np.log(xs) * scale
+    sm, dsm = fd_softmin_e(ls)
+    sr = np.exp(sm / scale)
+    dsr = sr * dsm / xs
+    return sr, dsr
+
+# average of xs weighted with softmax(-xs), i.e. the closeness to the minimum
+# this function is translation invariant
+def softmin_e(xs):
+    smax = softmax(-xs)
+    return smax @ xs
+
+def fd_softmin_e(xs):
+    smax = softmax(-xs)
+    smin = smax @ xs
+    return smin, smax * (1+smin - xs)
+
+# the classical softmax function
+def softmax(xs):
+    e = np.exp(xs)
+    smax = e / np.sum(e)
+    return smax
+
+from optimizers import Rprop
 
 class Problem:
-    def __init__(self, sqra, dts):
+    def __init__(self, sqra, dts, penalty=0.00001, g0=0.1, x0=None, maxsubder=1, optimizer=Rprop(), verbose=False):
         self.sqra = sqra
         self.dts = dts
+        self.penalty = penalty
+        self.maxsubder = maxsubder
+        self.verbose = verbose
+
+        if x0 is None:
+            x0 = np.zeros(self.sqra.N)
+
+        self.x = None
+
+        self.histx = []
+        self.histobj = []
+        self.histmin = []
+        self.histdobj = []
+        self.histdmin = []
+        self.histdcost = []
+
+
+        self.perturb(x0)
+
+        g0 = np.ones(self.sqra.N) * g0
+        self.optim = optimizer
+        self.optim.initialize(f=self.objcall, df=self.dobjcall, x0=self.x)
+
+
+    def objcall(self, x):
+        self.perturb(x)
+        return -self.obj
+    
+    def dobjcall(self, x):
+        self.perturb(x)
+        return -self.dobj
+
+    def print_status(self):
+        print(f"hp:{self.hpmin}, cost:{self.cost}, obj:{self.obj}")
+
 
     def perturb(self, x):
-        self.s = self.sqra.perturbed_copy(x)
+        self.x = x.copy()
+        self.s = self.sqra.perturbed(x)
         self.Qs = [self.s.Q] * len(self.dts)
         self.us = [self.s.u] * len(self.dts)
         self.hp = finite_time_hitting_prob_adjoint(self.Qs, self.dts, self.us)
-    
-    def obj(self, x):
-        self.perturb(x)
-        m, d = self.hp.min_and_derivative()
-        print(-m)
-        return m, d
 
-    def objwithpenalty(self, x, p):
-        m, d = self.obj(x)
-        return m + p*np.sum(np.abs(x)), d + np.sign(x) * p
+        #self.hpmin, self.dhpmin = self.hp.min_and_derivative()
+        self.hpmin, self.dhpmin = self.hp.relaxedminderivative(nmax=self.maxsubder)
+
+        self.cost = self.penalty * np.sum(np.abs(x))
+        self.dcost = self.penalty * np.sign(x)
+
+        self.obj  = self.hpmin  - self.cost
+        self.dobj = self.dhpmin - self.dcost
+
+
+        self.histx.append(self.x)
+        self.histobj.append(self.obj)
+        self.histmin.append(self.hpmin)
+        self.histdobj.append(self.dobj)
+        self.histdcost.append(self.dcost)
+        self.histdmin.append(self.dhpmin)
+
+        if self.verbose:
+            self.print_status()
+
+    def plot_gradient(self, penalty=True):
+        g = self.dhpmin
+        if penalty:
+            g = self.dobj
+
+        self.imshow(g, center=True)
+
+    def plot_x(self):
+        plt.title(f"hp:{self.hpmin:.4} cost:{self.cost:.4}")
+        self.imshow(self.x, center=True)
+
+    def plot_g(self):
+        self.imshow(self.optim.g)
+
+    def plot_u(self):
+        self.imshow(self.s.u)
+
+    def plot_xhist(self):
+        plt.plot(np.array(self.histx))
+        plt.legend(range(len(self.x)))
+        plt.show()
+
+    def plot_objhist(self):
+        plt.plot(np.array(self.histobj))
+        plt.plot(np.array(self.histmin))
+        plt.show()
+
+    def plot_derivatives(self):
+        plt.plot(self.histdobj)
+        plt.legend(range(len(self.x)))
+        plt.show()
+
+    def imshow(self, x, center=False):
+        if center:
+            m = np.max(np.abs(x))
+            vmin, vmax = -m, m
+            cmap = cmap=plt.get_cmap('bwr')
+        else:
+            vmin, vmax = np.min(x), np.max(x)
+            cmap = cmap=plt.get_cmap('cividis')
+
+        plt.imshow(np.reshape(x, self.sqra.dims), cmap=cmap, vmin=vmin, vmax=vmax)
+        plt.colorbar()
