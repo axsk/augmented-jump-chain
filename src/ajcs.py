48cd5b4debd500709a45b29439dfc27a678abe58
axsk <axsk@users.noreply.github.com>
2020-11-25 02:23:12 +0100

    WIP: refactor #1

--- src/ajcs.py
+++ src/ajcs.py
@@ -5,7 +5,6 @@ also provides finite hitting time computations via the space-time committor appr
 """
 
 
-import ajc
 import scipy.sparse as sp
 import numpy as np
 from scipy.sparse.linalg import spsolve
@@ -14,6 +13,9 @@ from scipy.optimize import minimize
 
 
 class AJCS():
+    """ sparse implementation of the Galerkin discretization to the AJC as in the preprint 
+    uses the """
+
     def __init__(self, Q, dt):
         self.nx = Q[0].shape[0]
         self.nt = len(dt)
@@ -26,31 +28,7 @@ class AJCS():
         self.qt, self.qi = self.qtilde(self.Q)
         self.k, self.H, self.S = self.jumpkernel(self.qt, self.qi, self.dt)
 
-    @property
-    def km(self):
-        if not hasattr(self, "_km"):
-            self._km = flatten_spacetime(self.k)
-        return self._km
-
-    def jump(self, p):
-        return np.reshape(p.flatten() @ self.km, p.shape)
-
-    def geiger(self, p, n=100):
-        g = np.copy(p)
-        pp = p
-        for i in range(n):
-            pp = self.jump(pp)
-            g = g + pp
-        return g
-
-    def holding_probs(self):
-        S = (1 - np.cumsum(self.S, axis=1))
-        S = np.moveaxis(np.triu(np.moveaxis(S, 2, 0)), 0, 2)
-        return S
-
-    def synchronize(self, p):
-        return np.einsum('sti, is -> it', self.holding_probs(), p)
-
+    
     @staticmethod
     def jumpkernel(qt, qi, dt):
         """ compute the galerkin discretization of the jump kernel eq[50] """
@@ -75,6 +53,128 @@ class AJCS():
                 # is  csr_scale_rows faster?
         return J, H, S
 
+    @staticmethod
+    def qtilde(Qs):
+        """ given an array of rate matrices compute the jump chain matrix
+        qt[i,j,t] = q^tilde_ij(t) : the row normalized jump matrix [eq. 14]
+        qi[i,t] = q_i(t): the outflow rate [eq. 6]
+        """
+
+        qt = [sp.dok_matrix(Q) for Q in Qs]
+        nt = len(qt)
+        nx = qt[0].shape[0]
+        qi = np.zeros((nt, nx))
+        for k in range(nt):
+            qt[k][range(nx), range(nx)] = 0
+            qi[k, :] = qt[k].sum(axis=1).A[:, 0]
+            qt[k] = sp.diags(1/qi[k, :]).dot(qt[k])
+            z = np.where(qi[k, :] == 0)           # special case q_i = 0 => q_ij = kron_ij
+            qt[k][z[0], :] = 0
+            qt[k][z[0], z[0]] = 1
+
+        return qt, qi.T
+
+    @property
+    def km(self):
+        if not hasattr(self, "_km"):
+            self._km = flatten_spacetime(self.k) # TODO: find a good way to deal with differing representations
+        return self._km
+
+    # TODO: compute this without falling back to the kernel matrix
+    def jump(self, p):
+        return np.reshape(p.flatten() @ self.km, p.shape)
+
+    def geiger(self, p, n=100):
+        g = np.copy(p)
+        pp = p
+        for i in range(n):
+            pp = self.jump(pp)
+            g = g + pp
+        return g
+
+    def holding_probs(self):
+        S = (1 - np.cumsum(self.S, axis=1))
+        S = np.moveaxis(np.triu(np.moveaxis(S, 2, 0)), 0, 2)
+        return S
+
+    def synchronize(self, p):
+        return np.einsum('sti, is -> it', self.holding_probs(), p)
+
+    ### SOLVERS FOR THE LINEAR SYSTEM 
+class AugmentedSolver:
+    """ methods for solving the augmented linear system 
+    Ax = b where A is upper triangular block 
+    where K[i,j] containing the blocks and b[i] the corresponding RHS """
+
+    def __init__(self, K):
+        self.K = K
+        self.nt = np.size(K, axis=0)
+        self.nx = np.size(K[0,0], axis=0)
+
+    @classmethod
+    def backwardsolve(self, K, b):
+        nt = np.size(K, axis=0)
+        b = b.copy()
+        q = np.zeros(np.shape(b))
+
+        for s in range(nt)[::-1]:
+            for t in np.arange(s, nt)[::-1]:
+                if s < t:
+                    b[s] -= K[s, t] @ q[t]
+                elif s == t:
+                    q[s] = spsolve(K[s, s], b[s])
+        return q
+
+    def koopman_system_one(self, i):
+        """ solve the koopman system for a single target state """
+        nx, nt = self.nx, self.nt
+
+        K = - deepcopy(self.k)
+        b = np.zeros((nt, nx))
+        S = self.holding_probs()[:,-1,:]
+
+
+        for s in range(nt):
+            K[s,s] += sp.identity(nx)
+            b[s][i] = S[s,i]
+
+        K[-1,-1] = sp.identity(nx)
+        b[-1][:] = 0
+        b[-1][i] = 1
+
+        q = self.backwardsolve(K, b)
+        return q
+
+    def koopman_system_iterated(self):
+        """ solve the koopman system for a all target states by solving individually for each """
+        nx = self.nx
+        K = np.zeros((nx, nx))
+        for i in range(nx):
+            K[:, i] = self.koopman_system_one(i)[0,:]
+        return K
+
+    def koopman_system(self):
+        return self.koopman_system_all()[0,:,:]
+
+    def koopman_system_all(self):
+        """ solve the koopman system for all target states together """
+        nx, nt = self.nx, self.nt
+        K = - deepcopy(self.k)
+        b = np.zeros((nt, nx, nx))
+
+        S = self.holding_probs()[:,-1,:]
+
+        for s in range(nt):
+            K[s,s] += sp.identity(nx)
+            b[s] = np.diag(S[s, :])
+
+        K[-1,-1] = sp.identity(nx)
+
+        q = self.backwardsolve(K, b)
+        return q
+
+    """ HITTING PROBABILITIES """
+
     def finite_time_hitting_prob(self, state):
         """ Compute the probability to hit a given state n_state over the
         time horizon of the jump chain from any space-time point by solving
@@ -97,73 +197,13 @@ class AJCS():
         q = self.backwardsolve(K, b)
         return q
 
-    def backwardsolve(self, K, b):
-        nt = self.nt
-        b = b.copy()
-        q = np.zeros(np.shape(b))
-
-        for s in range(nt)[::-1]:
-            for t in np.arange(s, nt)[::-1]:
-                if s < t:
-                    b[s] -= K[s, t] @ q[t]
-                elif s == t:
-                    q[s] = spsolve(K[s, s], b[s])
-
-        return q
-
-    def koopman_system_one(self, i):
-        nx, nt = self.nx, self.nt
-
-        K = - deepcopy(self.k)
-        b = np.zeros((nt, nx))
-        S = self.holding_probs()[:,-1,:]
-
-
-        for s in range(nt):
-            K[s,s] += sp.identity(nx)
-            b[s][i] = S[s,i]
-
-        K[-1,-1] = sp.identity(nx)
-        b[-1][:] = 0
-        b[-1][i] = 1
-
-        q = self.backwardsolve(K, b)
-        return q
-
-    def koopman_system_iterated(self):
-        nx = self.nx
-        K = np.zeros((nx, nx))
-        for i in range(nx):
-            K[:, i] = self.koopman_system_one(i)[0,:]
-        return K
-
-    def koopman_system(self):
-        return self.koopman_system_all()[0,:,:]
-
-    def koopman_system_all(self):
-        nx, nt = self.nx, self.nt
-        K = - deepcopy(self.k)
-        b = np.zeros((nt, nx, nx))
-
-        S = self.holding_probs()[:,-1,:]
-
-        for s in range(nt):
-            K[s,s] += sp.identity(nx)
-            b[s] = np.diag(S[s, :])
-
-        K[-1,-1] = sp.identity(nx)
-
-        q = self.backwardsolve(K, b)
-        return q
-
-
-
     def finite_time_hitting_probs(self):
         """ finite_time_hitting_probs[i,j] is the probability to hit state j starting in i in the time window of the process """
         # TODO: check if this is indeed the ordering
         return np.vstack([self.finite_time_hitting_prob(i)[0,:] for i in range(self.nx)]).T
 
     def optimize(self, iters=100, penalty=0, x0=None, adaptive=False):
+        """ gradient free optimization of the minimal finite time hitting prob """
         # TODO: assert identical sparsity structures
         j = self
         jp = deepcopy(self)
@@ -187,29 +227,11 @@ class AJCS():
         obj(res.x)
         return res, jp
 
-    @staticmethod
-    def qtilde(Qs):
-        """ given a standard rate matrix returns:
-        qt[i,j,t] = q^tilde_ij(t) : the row normalized jump matrix [eq. 14]
-        qi[i,t] = q_i(t): the outflow rate [eq. 6]
-        """
-
-        qt = [sp.dok_matrix(Q) for Q in Qs]
-        nt = len(qt)
-        nx = qt[0].shape[0]
-        qi = np.zeros((nt, nx))
-        for k in range(nt):
-            qt[k][range(nx), range(nx)] = 0
-            qi[k, :] = qt[k].sum(axis=1).A[:, 0]
-            qt[k] = sp.diags(1/qi[k, :]).dot(qt[k])
-            z = np.where(qi[k, :] == 0)           # special case q_i = 0 => q_ij = kron_ij
-            qt[k][z[0], :] = 0
-            qt[k][z[0], z[0]] = 1
-
-        return qt, qi.T
 
 
 
+# TODO: this should also work for vectors
+# we might want something to convert between stacked K[s,t][i,j], flattened [st, ij] and tensor [s,i,t,j]
 def flatten_spacetime(tensor):
     ns, nt = tensor.shape
     for s in range(ns):
